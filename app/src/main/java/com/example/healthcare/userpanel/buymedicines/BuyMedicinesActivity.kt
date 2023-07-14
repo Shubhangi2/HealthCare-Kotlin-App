35d2a2393bb1e55aea03c892d9e1ce31ff665267
Shubhangi Jadhav <jshubhangi958@gmail.com>
2023-07-14 13:39:59 +0530

    bug fixed

---
+++ app/src/main/java/com/example/healthcare/userpanel/buymedicines/BuyMedicinesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.healthcare.userpanel.buymedicines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.healthcare.R
+
+class BuyMedicinesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_medicines)
+    }
+}
